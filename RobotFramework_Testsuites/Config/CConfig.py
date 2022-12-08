fc79ea16fde00ed91685437ae9a56b0a5077b082
mas2hc <son.maidinhnam@vn.bosch.com>
2022-12-08 11:36:34 +0700

    Fixed typo issue according to the review

--- RobotFramework_Testsuites/Config/CConfig.py
+++ RobotFramework_Testsuites/Config/CConfig.py
@@ -576,7 +576,7 @@ another parameter in local config file is prohibited!"
         except Exception as error:
             CConfig.bLoadedCfg = False
             CConfig.sLoadedCfgError = str(error)
-            logger.error("Loading of JSON configuration file failed! Reason: %s" %(CConfig.sLoadedCfgError))
+            logger.error(f"Loading of JSON configuration file failed! Reason: %s" %(CConfig.sLoadedCfgError))
             raise Exception
         
         try:
@@ -584,8 +584,8 @@ another parameter in local config file is prohibited!"
             self.sTestCfgFile = oSuiteConfig[self.sConfigName]['name']
             sTestCfgDir = oSuiteConfig[self.sConfigName]['path']
         except:
-            CConfig.sLoadedCfgError = "Testsuite management - Loading configuration level 2 is failed! \n \
-                The vairant '%s' is not defined in '%s'" % (self.sConfigName, os.path.abspath(self.sTestSuiteCfg))
+            CConfig.sLoadedCfgError = f"Testsuite management - Loading configuration level 2 failed! \n \
+                The variant '%s' is not defined in '%s'" % (self.sConfigName, os.path.abspath(self.sTestSuiteCfg))
             logger.error(CConfig.sLoadedCfgError)
             return
             
@@ -602,7 +602,7 @@ another parameter in local config file is prohibited!"
                         bFoundTestCfgDir = True
                         break
                 if bFoundTestCfgDir == False:
-                    raise Exception('Could not find out config directory: %s' %(sTestCfgDirStart))
+                    raise Exception(f"Could not find out config directory: %s" %(sTestCfgDirStart))
                 
         self.sTestCfgFile = sTestCfgDir + self.sTestCfgFile
 
