3484f2d8c150a3f261aaf8ede9b8bfd78421c498
mar3hc <tri.maiminh@vn.bosch.com>
2025-12-08 14:27:20 +0700

    migrate from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -183,47 +183,16 @@ if ( ('install' in listCmdArgs) or ('build' in listCmdArgs) or ('sdist' in listC
 # --------------------------------------------------------------------------------------------------------------
 
 # -- the 'setup' itself
+# NOTE: Metadata now comes from pyproject.toml
+# This minimal setup.py only runs custom build steps and delegates to pyproject.toml
 
-print(COLBY + "Extended setup step 5/5: install.run(self)")
+print(COLBY + "Extended setup step 5/5: Running setup with pyproject.toml configuration")
 print()
 
 setuptools.setup(
-    name         = str(oRepositoryConfig.Get('REPOSITORYNAME')),
-    version      = str(oRepositoryConfig.Get('PACKAGEVERSION')),
-    author       = str(oRepositoryConfig.Get('AUTHOR')),
-    author_email = str(oRepositoryConfig.Get('AUTHOREMAIL')),
-    description  = str(oRepositoryConfig.Get('DESCRIPTION')),
-    long_description = long_description,
-    long_description_content_type = str(oRepositoryConfig.Get('LONGDESCRIPTIONCONTENTTYPE')),
-    url = str(oRepositoryConfig.Get('URL')),
-
-    packages = [str(oRepositoryConfig.Get('PACKAGENAME')),
-                str(oRepositoryConfig.Get('PACKAGENAME')) + ".Config",
-                str(oRepositoryConfig.Get('PACKAGENAME')) + ".Keywords",
-                str(oRepositoryConfig.Get('PACKAGENAME')) + ".Utils",
-                str(oRepositoryConfig.Get('PACKAGENAME')) + ".Utils.Events"],
-
-    package_dir = {str(oRepositoryConfig.Get('REPOSITORYNAME')) : str(oRepositoryConfig.Get('PACKAGENAME'))},
-
-    include_package_data = True,
-
-    classifiers = [
-        str(oRepositoryConfig.Get('PROGRAMMINGLANGUAGE')),
-        str(oRepositoryConfig.Get('LICENCE')),
-        str(oRepositoryConfig.Get('OPERATINGSYSTEM')),
-        str(oRepositoryConfig.Get('DEVELOPMENTSTATUS')),
-        str(oRepositoryConfig.Get('INTENDEDAUDIENCE')),
-        str(oRepositoryConfig.Get('TOPIC')),
-    ],
-    python_requires = str(oRepositoryConfig.Get('PYTHONREQUIRES')),
     cmdclass={
         'install': ExtendedInstallCommand,
     },
-    install_requires = oRepositoryConfig.Get('INSTALLREQUIRES'),
-    entry_points={
-        'console_scripts': oRepositoryConfig.Get('CONSOLESCRIPTS'),
-    },
-    package_data={f"{oRepositoryConfig.Get('PACKAGENAME')}" : oRepositoryConfig.Get('PACKAGEDATA')},
 )
 # --------------------------------------------------------------------------------------------------------------
 
